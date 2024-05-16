a82686564d0b4e296fedf6a590ddb57e20b51509
Ram <ram440163@gmail.com>
2024-05-16 15:08:29 +0530

    newly added

--- src/PriceCard-Task.jsx
+++ src/PriceCard-Task.jsx
@@ -1,91 +1,92 @@
 import "./PriceCard.css"
 const data = [
-  {
-      choice   : "FREE",
-      title    : "$0/month",
-      features : [
-          {offer: "Single User", isEnabled: true},
-          {offer: "50GB Storage", isEnabled: true},
-          {offer: "Unlimited Public Projects", isEnabled: true},
-          {offer: "Community Access", isEnabled: true},
-          {offer: "Unlimited Private Projects", isEnabled: false},
-          {offer: "Dedicated Phone Support", isEnabled: false},
-          {offer: "Free Subdomain", isEnabled: false},
-          {offer: "Monthly Status Reports", isEnabled: false},       
-      ],
-  },
+    {
+        choice: "FREE",
+        title: "$0/month",
+        features: [
+            { offer: "Single User", isEnabled: true },
+            { offer: "50GB Storage", isEnabled: true },
+            { offer: "Unlimited Public Projects", isEnabled: true },
+            { offer: "Community Access", isEnabled: true },
+            { offer: "Unlimited Private Projects", isEnabled: false },
+            { offer: "Dedicated Phone Support", isEnabled: false },
+            { offer: "Free Subdomain", isEnabled: false },
+            { offer: "Monthly Status Reports", isEnabled: false },
+        ],
+    },
 
-  {
-      choice   : "PLUS",
-      title    : "$9/month",
-      features : [
-          {offer: "5 Users", isEnabled: true},
-          {offer: "50GB Storage", isEnabled: true},
-          {offer: "Unlimited Public Projects", isEnabled: true},
-          {offer: "Community Access", isEnabled: true},
-          {offer: "Unlimited Private Projects", isEnabled: true},
-          {offer: "Dedicated Phone Support", isEnabled: true},
-          {offer:"Free Subdomain", isEnabled: true},
-          {offer: "Monthly Status Reports", isEnabled: false},       
-      ],
-  },
+    {
+        choice: "PLUS",
+        title: "$9/month",
+        features: [
+            { offer: "5 Users", isEnabled: true },
+            { offer: "50GB Storage", isEnabled: true },
+            { offer: "Unlimited Public Projects", isEnabled: true },
+            { offer: "Community Access", isEnabled: true },
+            { offer: "Unlimited Private Projects", isEnabled: true },
+            { offer: "Dedicated Phone Support", isEnabled: true },
+            { offer: "Free Subdomain", isEnabled: true },
+            { offer: "Monthly Status Reports", isEnabled: false },
+        ],
+    },
 
-  {
-      choice   : "PRO",
-      title    : "$49/Month",
-      features : [
-          {offer: "Unlimited Users", isEnabled: true},
-          {offer: "50GB Storage", isEnabled: true},
-          {offer: "Unlimited Public Projects", isEnabled: true},
-          {offer: "Community Access", isEnabled: true},
-          {offer: "Unlimited Private Projects", isEnabled: true},
-          {offer: "Dedicated Phone Support", isEnabled: true},
-          {offer: "Free Subdomain", isEnabled: true},
-          {offer: "Monthly Status Reports", isEnabled: true},       
-      ],
-  },
+    {
+        choice: "PRO",
+        title: "$49/Month",
+        features: [
+            { offer: "Unlimited Users", isEnabled: true },
+            { offer: "50GB Storage", isEnabled: true },
+            { offer: "Unlimited Public Projects", isEnabled: true },
+            { offer: "Community Access", isEnabled: true },
+            { offer: "Unlimited Private Projects", isEnabled: true },
+            { offer: "Dedicated Phone Support", isEnabled: true },
+            { offer: "Free Subdomain", isEnabled: true },
+            { offer: "Monthly Status Reports", isEnabled: true },
+        ],
+    },
 ];
 
-const Subscriptions = ()=>{
-  return(
-    
-      <div 
-      style={{
-          backgroundcolor: "skyblue",
-          margin: "10px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          
-      }}
+const Subscriptions = () => {
+    return (
 
-       >
-        
-       {data.map((Val)=>(
-        
-       <div key={Val.title} 
-        style={{border:"18px solid #0265e7", margin: "15px", padding: "25px"}}
+        <div
+            style={{
+                backgroundcolor: "skyblue",
+                margin: "10px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+
+            }}
+
         >
-        <p>{Val.choice}</p>
-        <h2>{Val.title}</h2>
 
-        {Val.features.map((feat)=>(
-          <p key={feat.offer} style={{textDecoration: feat.isEnabled ?"none":"line-through", }}
-          >
-           {feat.isEnabled &&  <i className="fa-solid fa-check"></i>}
-           {!feat.isEnabled && <i className="fa-solid fa-x"></i>} 
-           {" "}
-           {feat.offer}</p>
+            {data.map((Val) => (
 
-        ))};
+                <div key={Val.title}
+                    style={{ border: "18px solid #0265e7", margin: "15px", padding: "30px" }}
+                >
+                    <p>{Val.choice}</p>
+                    <h2>{Val.title}</h2>
 
-        <div>
-          <button className="btn">button</button>
+                    {Val.features.map((feat) => (
+                        <p key={feat.offer}
+                        >
+
+                            {feat.isEnabled && <i className="fa-solid fa-check"></i>}
+                            {!feat.isEnabled && <i className="fa-solid fa-x"></i>}
+                            {" "}
+                            {feat.offer}</p>
+
+                    ))}
+
+                    <div>
+                        <button className="btn">button</button>
+                    </div>
+                </div>
+
+            ))}
         </div>
-       </div>
-        
-       ))};
-         </div>
-  );
-  };
-  export default Subscriptions;
+    )
+};
+export default Subscriptions;
